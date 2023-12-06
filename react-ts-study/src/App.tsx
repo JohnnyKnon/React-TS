0340d3a6eaec602cecec0dfd38436a67bb4d23c5
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-06 18:02:59 +0900

    작업 시나리오 설정과 데이터 작성 + 컴포넌트 생성

--- react-ts-study/src/App.tsx
+++ react-ts-study/src/App.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+/**
+ * 작업 시나리오 -> 레스토랑
+ * 
+ */
+
+const data = {
+  name : '심야식당',
+  category:'Japanese',
+  address : {
+    city : 'Tokyo',
+    detail: 'somewhere',
+    zipcode: 12377
+  },
+  menu:[{name:'sushi',price:15000, category: 'SUSHI'}, {name:'ramen',price:5000, category: 'RAMEN'},{name:'sake onigiri', price:1500, category: 'ONIGIRI'} ]
+}
+
 const App:React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
