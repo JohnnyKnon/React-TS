91073cfe2b54c46daa869c9936c78087cabd4704
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-06 18:34:05 +0900

    type 으로 interface extends 처럼 사용하기

--- react-ts-study/src/components/BestMenu.tsx
+++ react-ts-study/src/components/BestMenu.tsx
@@ -3,7 +3,11 @@ import { Menu } from '../model/restaurant';
 
 
 // extends를 통해서 해당 타입이 보이지는 않지만 그대로 들어가져 있음
-interface BestMenuProps extends Menu {
+// interface BestMenuProps extends Menu {
+//     onShowBestMenuName(name:string):string 
+// }
+
+type BestMenuProps = Menu & {
     onShowBestMenuName(name:string):string 
 }
 
