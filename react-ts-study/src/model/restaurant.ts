2eb094630a29948be059594d729acc88ad7571a7
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-06 18:12:32 +0900

    Type을 세분화 시키고 연동해주기

--- react-ts-study/src/model/restaurant.ts
+++ react-ts-study/src/model/restaurant.ts
@@ -10,17 +10,25 @@
 //   }
   
 // 타입은 interface or type 으로 작업 가능 Syntax랑 방식이 조금 다름
+
+// 레스토랑 기본 데이터 타입
 export type Restaurant = {
     name:string;
     category:string;
-    address:{
-        city:string;
-        detail:string;
-        zipcode:number;
-    };
-    menu:{
-        name:string;
-        price:number;
-        category:string;
-    }[];
+    address:Address;
+    menu:Menu[];
+}
+
+// 주소 타입
+export type Address = {
+    city:string;
+    detail:string;
+    zipcode:number;
+}
+
+// 메뉴타입
+export type Menu = {
+    name:string;
+    price:number;
+    category:string;
 }
